61e6c8b4b68090dee884b7317bc19fb55265b779
fujimoto <notinitotiminitimikani.1770297@gmail.com>
2021-04-09 10:01:13 +0900

    seventeenモジュールの実装

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
+/**
+ * 17の倍数であるかどうか判定する
+ * @param {number} num
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
